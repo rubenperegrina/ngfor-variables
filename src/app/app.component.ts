10b4361f20e90501bedcb6186fa5a692a5b57fe8
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-06-27 11:42:18 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [NgFor],
+  template:
+    `<div *ngFor="let user of users; index as i; first as isFirst; last as isLast; odd as isOdd; even as isEven; count as isCount">
+      {{ i + '-'}}
+      {{ user }}
+      {{ isFirst ? ' - first' : '' }}
+      {{ isLast ? ' - last' : '' }}
+      {{ isOdd ? ' - odd' : '' }}
+      {{ isEven ? ' - even' : '' }}
+      {{ isCount ? ' - count' : '' }}
+  </div>`
 })
 export class AppComponent {
-  title = 'ngform-variables';
+  users = ['Bob', 'Peter', 'Jane', 'John'];
 }
